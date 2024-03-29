7b549c596c1c053acfb03c089e5702cd2b2b8bcf
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-29 19:33:50 +0700

    update Mix/mix_client.py

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -4,10 +4,10 @@
  
  @ CREDIT : NAN-DEV
 """
-import asyncio
-import importlib
 ################################################################
 import logging
+import asyncio
+import importlib
 import os
 import re
 import shlex
@@ -20,7 +20,7 @@ from pyrogram.enums import *
 from pyrogram.errors import *
 from pyrogram.handlers import *
 from pyrogram.types import *
-from team.nandev.class_log import LOGGER
+from team.nandev.class_log import LOGG, LOGGER
 from team.nandev.class_modules import CMD_HELP
 from team.nandev.database import ndB, udB
 
@@ -297,9 +297,9 @@ class ConnectionHandler(logging.Handler):
             if ah in record.getMessage():
                 os.execl(sys.executable, sys.executable, "-m", "Mix")
 
-
+logger = logging.getLogger()
 connection_handler = ConnectionHandler()
-logging.addHandler(connection_handler)
+logger.addHandler(connection_handler)
 
 
 class Bot(Client):
