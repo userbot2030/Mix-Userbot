c17b4897320c80a8b7bba10d16ea271c9a3466d4
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-04-02 16:54:23 +0700

    update Mix/core/gclog.py

--- Mix/core/gclog.py
+++ Mix/core/gclog.py
@@ -19,7 +19,7 @@ from pyrogram import __version__ as pyrover
 from pyrogram.errors import *
 from pyrogram.types import ChatPrivileges
 from pytgcalls import __version__ as pytgver
-# from team.nandev.class_handler import TAG_LOG
+from team.nandev.class_handler import TAG_LOG
 from team.nandev.class_log import LOGGER
 from team.nandev.class_modules import CMD_HELP
 from team.nandev.database import ndB
@@ -27,40 +27,38 @@ from team.nandev.database import ndB
 from config import *
 from Mix import bot, nlx
 
-ANAK_KONTOL = ndB.get_key("TAG_LOG") or log_channel
-
 
 async def check_logger():
-    # LOGS("Check Grup Log...")
-    if ANAK_KONTOL is not None:
+    if not ndB.get_key("TAG_LOG") and log_channel is None:
+        LOGGER.info(f"Creating Grup Log...")
+        nama = f"Mix-Userbot Logs"
+        des = "Jangan Keluar Dari Grup Log Ini\n\nPowered by: @KynanSupport"
+        log_pic = "https://telegra.ph//file/ee7fc86ab183a0ff90392.jpg"
+        gc = await nlx.create_supergroup(nama, des)
+        bhan = wget.download(f"{log_pic}")
+        gmbr = {"video": bhan} if bhan.endswith(".mp4") else {"photo": bhan}
+        kntl = gc.id
+        await nlx.set_chat_photo(kntl, **gmbr)
+        await nlx.promote_chat_member(
+            kntl,
+            bot.me.username,
+            privileges=ChatPrivileges(
+                can_change_info=True,
+                can_invite_users=True,
+                can_delete_messages=True,
+                can_restrict_members=True,
+                can_pin_messages=True,
+                can_promote_members=True,
+                can_manage_chat=True,
+                can_manage_video_chats=True,
+            ),
+        )
+        ndB.set_key("TAG_LOG", kntl)
+        await nlx.send_message(kntl, f"<b>Group Log Berhasil Dibuat.</b>")
+        LOGGER.info(f"Group Logger Enable...")
+        execvp(executable, [executable, "-m", "Mix"])
+    else:
         return
-    LOGGER.info(f"Creating Grup Log...")
-    nama = f"Mix-Userbot Logs"
-    des = "Jangan Keluar Dari Grup Log Ini\n\nPowered by: @KynanSupport"
-    log_pic = "https://telegra.ph//file/ee7fc86ab183a0ff90392.jpg"
-    gc = await nlx.create_supergroup(nama, des)
-    bhan = wget.download(f"{log_pic}")
-    gmbr = {"video": bhan} if bhan.endswith(".mp4") else {"photo": bhan}
-    kntl = gc.id
-    await nlx.set_chat_photo(kntl, **gmbr)
-    await nlx.promote_chat_member(
-        kntl,
-        bot.me.username,
-        privileges=ChatPrivileges(
-            can_change_info=True,
-            can_invite_users=True,
-            can_delete_messages=True,
-            can_restrict_members=True,
-            can_pin_messages=True,
-            can_promote_members=True,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-        ),
-    )
-    ndB.set_key("TAG_LOG", kntl)
-    await nlx.send_message(kntl, f"<b>Group Log Berhasil Dibuat.</b>")
-    LOGGER.info(f"Group Logger Enable...")
-    execvp(executable, [executable, "-m", "Mix"])
 
 
 async def getFinish():
@@ -68,7 +66,7 @@ async def getFinish():
     xx = " ".join(emut)
     try:
         await bot.send_message(
-            int(ANAK_KONTOL),
+            int(TAG_LOG),
             f"""
 <b>Userbot Successfully Deploy !!</b>
 
@@ -82,7 +80,7 @@ async def getFinish():
     except (ChannelInvalid, PeerIdInvalid):
         try:
             await nlx.promote_chat_member(
-                int(ANAK_KONTOL),
+                int(TAG_LOG),
                 bot.me.username,
                 privileges=ChatPrivileges(
                     can_change_info=True,
@@ -96,7 +94,7 @@ async def getFinish():
                 ),
             )
             await bot.send_message(
-                int(ANAK_KONTOL),
+                int(TAG_LOG),
                 f"""
 <b>Userbot Successfully Deploy !!</b>
 
